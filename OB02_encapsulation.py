edf0e57589d8a7fb6b9f95736ad9a568e2fbdceb
YuriyNik156 <yuriy.nik156@gmail.com>
2025-03-03 20:21:52 +0300

    Create OB02_encapsulation.py
    Создан OB02_encapsulation.py

---
+++ OB02_encapsulation.py
@@ -0,0 +1,20 @@
+class Test():
+    def public_func(self):
+        print("Публичный метод")
+
+    def _protected_func(self):
+        print("Защищенный метод")
+
+    def __private_func(self):
+        print("Приватный метод")
+
+    def test_private(self):
+        self.__private_func()
+
+test = Test()
+
+test.public_func()
+
+test._protected_func()
+
+test.test_private()
